764ff4565cae0dc3249527e066584eb28342c41e
dincim <dincmurad@gmail.com>
2019-06-17 22:58:52 -0700

    new project

---
+++ src/emirfirsthello/Addition.java
@@ -0,0 +1,12 @@
+package emirfirsthello;
+
+public class Addition {
+
+	public static void main(String[] args) {
+
+		Calculator add1 = new Calculator();
+		add1.add();
+
+	}
+
+}
---
+++ src/emirfirsthello/Calculator.java
@@ -0,0 +1,13 @@
+package emirfirsthello;
+
+public class Calculator {
+	
+	 int x = 2;
+	 int y = 2;
+	 
+
+	public void add() {
+		System.out.println(x+y);	
+	}
+
+}
